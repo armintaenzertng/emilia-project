7216bc5c684230fb0fde19b92028ef7851db7cbc
Armin Tänzer <arminotaurus09@gmail.com>
2024-02-09 11:44:31 +0100

    extract state polling to not poll for every lamp individually

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,45 @@
 "use client"
 
 import StateButton from '../components/StateButton';
+import {useEffect, useState} from "react";
 
 export default function Home() {
+    
+    // This polls for the current state of the lamps at regular intervals. The state is shared by all lamps, their bitPosition determines if they are on or off.
+    const [binaryState, setBinaryState] = useState(0)
+    useEffect(() => {
+        const interval = setInterval(async () => {
+            const response = await fetch('/api/state', {method: 'GET'});
+            const data = await response.json();
+            setBinaryState(data.lightState)
+        }, 500); // Poll regularly (value in milliseconds)
 
+        return () => clearInterval(interval);
+    }, []);
+    
+    
     return (
         <>
             <div>
-                <StateButton bitPosition={0}/>
+                <StateButton bitPosition={0} binaryState={binaryState}/>
             </div>
             <div>
-                <StateButton bitPosition={1}/>
+                <StateButton bitPosition={1} binaryState={binaryState}/>
             </div>
             <div>
-                <StateButton bitPosition={2}/>
+                <StateButton bitPosition={2} binaryState={binaryState}/>
             </div>
             <div>
-                <StateButton bitPosition={3}/>
+                <StateButton bitPosition={3} binaryState={binaryState}/>
             </div>
             <div>
-                <StateButton bitPosition={4}/>
+                <StateButton bitPosition={4} binaryState={binaryState}/>
             </div>
             <div>
-                <StateButton bitPosition={5}/>
+                <StateButton bitPosition={5} binaryState={binaryState}/>
             </div>
             <div>
-                <StateButton bitPosition={6}/>
+                <StateButton bitPosition={6} binaryState={binaryState}/>
             </div>
         </>
     );
--- components/StateButton.jsx
+++ components/StateButton.jsx
@@ -1,11 +1,10 @@
 import {useEffect, useState} from "react";
 
 
-export default function StateButton({ bitPosition }) {
-    let binaryState = 0
-    
+export default function StateButton({bitPosition, binaryState}) {
+
     const [active, setActive] = useState(false);
-    
+
     const buttonStyle = {
         backgroundColor: active ? 'green' : 'red',
         color: 'white',
@@ -15,37 +14,28 @@ export default function StateButton({ bitPosition }) {
         cursor: 'pointer',
     };
 
+    useEffect(() => {
+        console.log("frontend: binaryState for button " + bitPosition + " is " + binaryState)
+        
+        setActive(isNthBitSet(binaryState, bitPosition));
+    }, [binaryState])
+
     const isNthBitSet = (binaryState, n) => {
         return ((binaryState >> n) & 1) === 1;
     };
 
-    // TODO: this polls for every button even though the data.lightState is the same for all buttons
-    useEffect(() => {
-        const interval = setInterval(async () => {
-            const response = await fetch('/api/state', {method: 'GET'});
-            const data = await response.json();
-            setActive(isNthBitSet(data.lightState, bitPosition));
-        }, 500); // Poll every 500 milliseconds
-
-        return () => clearInterval(interval);
-    }, []);
-
-    const toggleActive = async (lightNumber) => {
-        console.log("toggleActive has been activated")
+    const sendToggleCommand = async (lightNumber) => {
         const response = await fetch('/api/state', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ n: lightNumber }),
+            body: JSON.stringify({n: lightNumber}),
         });
-        const data = await response.json();
-        binaryState = data.lightState;
-        console.log("frontend: binaryState for button " + bitPosition + " is " + binaryState)
     };
-    
+
     return (
-        <button style={buttonStyle} onClick={() => toggleActive(bitPosition)}>
+        <button style={buttonStyle} onClick={() => sendToggleCommand(bitPosition)}>
             {active ? 'Active' : 'Inactive'}
         </button>
     );
